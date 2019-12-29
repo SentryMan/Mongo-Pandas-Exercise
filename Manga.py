a3eab40a9ee827172c9e23fc25c630d36a9da38c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-12-28 21:32:02 -0600

    Update Manga.py

--- Manga.py
+++ Manga.py
@@ -5,10 +5,12 @@ Created on Sat Dec 28 19:47:05 2019
 @author: Yutyrannus
 """
 
+import csv
 import pymongo
 import pandas as pd
 from pymongo import MongoClient
 
+"Setting up Mongo"
 client = MongoClient('mongodb+srv://jojo:weak@cluster0-ua5e8.mongodb.net')
 db = client['MangaDB']
 
@@ -16,4 +18,18 @@ col = db.Manga
 
 mdf = pd.DataFrame(list(col.find()))
 
-mdf = mdf.drop(['_id', '_class'], axis=1)
+"Drop data we won't need"
+mdf = mdf.drop(['_id', 'i', '_class'], axis=1)
+
+nulls = mdf[mdf.RealID == -1]
+nulls.info()
+
+mdf = mdf[mdf.RealID != -1]
+mdf.info()
+
+
+
+
+"Turns it into csv"
+mdf.to_csv('manga.csv', index=False)
+mcsv = pd.read_csv('manga.csv')
